113119079bfd5aad96cd002688d73a376fc50794
Jarizoon <86997361+Jarizoon@users.noreply.github.com>
2022-07-03 20:22:30 +0700

    login

---
+++ SalesWinApp/frmMain.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DataAccess.Repository;
+
+namespace SalesWinApp
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        public bool AdminOrMember;
+    }
+}
