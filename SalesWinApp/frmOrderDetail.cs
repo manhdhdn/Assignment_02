d4e7386f2939c63703a3efddb206aeaa348ab566
QuocLoc12 <nguyenloc120121@gmail.com>
2022-07-11 20:10:03 +0700

    add frmOrderDetailManagement, frmOrderDetail, frmOrderManagement, frmOrder

---
+++ SalesWinApp/frmOrderDetail.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BusinessObject;
+using DataAccess.Repository;
+
+namespace SalesWinApp
+{
+    public partial class frmOrderDetail : Form
+    {
+        public frmOrderDetail()
+        {
+            InitializeComponent();
+        }
+
+        public IOrderDetailRepository OrderDetailRepository { get; set; }
+        public bool InsertOrUpdate { get; set; }
+        public OrderDetail OrderDetailInfo { get; set; }
+
+        private void frmOrderDetail_Load(object sender, EventArgs e)
+        {
+            txtOrderID.Enabled = !InsertOrUpdate;
+            if (InsertOrUpdate == true)
+            {
+                txtOrderID.Text = OrderDetailInfo.OrderId.ToString();
+                txtProductID.Text = OrderDetailInfo.ProductId.ToString();
+                txtUnitPrice.Text = OrderDetailInfo.UnitPrice.ToString();
+                txtQuantity.Text = OrderDetailInfo.Quantity.ToString();
+                txtDiscount.Text = OrderDetailInfo.Discount.ToString();
+            }
+        }
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var orderDetail = new OrderDetail
+                {
+                    OrderId = int.Parse(txtOrderID.Text),
+                    ProductId = int.Parse(txtProductID.Text),
+                    UnitPrice = decimal.Parse(txtUnitPrice.Text),
+                    Quantity = int.Parse(txtQuantity.Text),
+                    Discount = double.Parse(txtDiscount.Text)
+                };
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, InsertOrUpdate == false ? "Add new order" : "Update order");
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e) => Close();
+
+
+    }
+}
---
+++ SalesWinApp/frmOrderManagement.Designer.cs
@@ -0,0 +1,305 @@
+﻿namespace SalesWinApp
+{
+    partial class frmOrderManagement
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvOrderList = new System.Windows.Forms.DataGridView();
+            this.btnView = new System.Windows.Forms.Button();
+            this.lbOrderID = new System.Windows.Forms.Label();
+            this.lbMemberID = new System.Windows.Forms.Label();
+            this.lbOrderDate = new System.Windows.Forms.Label();
+            this.lbRequireDate = new System.Windows.Forms.Label();
+            this.lbShippedDate = new System.Windows.Forms.Label();
+            this.lbFreight = new System.Windows.Forms.Label();
+            this.txtOrderID = new System.Windows.Forms.TextBox();
+            this.txtMemberID = new System.Windows.Forms.TextBox();
+            this.txtOrderDate = new System.Windows.Forms.TextBox();
+            this.txtRequiredDate = new System.Windows.Forms.TextBox();
+            this.txtShippedDate = new System.Windows.Forms.TextBox();
+            this.txtFreight = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnOrderDetail = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOrderList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvOrderList
+            // 
+            this.dgvOrderList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvOrderList.Location = new System.Drawing.Point(183, 146);
+            this.dgvOrderList.Name = "dgvOrderList";
+            this.dgvOrderList.ReadOnly = true;
+            this.dgvOrderList.RowTemplate.Height = 25;
+            this.dgvOrderList.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvOrderList.Size = new System.Drawing.Size(723, 241);
+            this.dgvOrderList.TabIndex = 0;
+            // 
+            // btnView
+            // 
+            this.btnView.Location = new System.Drawing.Point(183, 115);
+            this.btnView.Name = "btnView";
+            this.btnView.Size = new System.Drawing.Size(100, 23);
+            this.btnView.TabIndex = 1;
+            this.btnView.Text = "View Order List";
+            this.btnView.UseVisualStyleBackColor = true;
+            this.btnView.Click += new System.EventHandler(this.btnView_Click);
+            // 
+            // lbOrderID
+            // 
+            this.lbOrderID.AutoSize = true;
+            this.lbOrderID.Location = new System.Drawing.Point(12, 123);
+            this.lbOrderID.Name = "lbOrderID";
+            this.lbOrderID.Size = new System.Drawing.Size(51, 15);
+            this.lbOrderID.TabIndex = 2;
+            this.lbOrderID.Text = "Order ID";
+            // 
+            // lbMemberID
+            // 
+            this.lbMemberID.AutoSize = true;
+            this.lbMemberID.Location = new System.Drawing.Point(12, 172);
+            this.lbMemberID.Name = "lbMemberID";
+            this.lbMemberID.Size = new System.Drawing.Size(66, 15);
+            this.lbMemberID.TabIndex = 3;
+            this.lbMemberID.Text = "Member ID";
+            // 
+            // lbOrderDate
+            // 
+            this.lbOrderDate.AutoSize = true;
+            this.lbOrderDate.Location = new System.Drawing.Point(12, 216);
+            this.lbOrderDate.Name = "lbOrderDate";
+            this.lbOrderDate.Size = new System.Drawing.Size(64, 15);
+            this.lbOrderDate.TabIndex = 4;
+            this.lbOrderDate.Text = "Order Date";
+            // 
+            // lbRequireDate
+            // 
+            this.lbRequireDate.AutoSize = true;
+            this.lbRequireDate.Location = new System.Drawing.Point(12, 260);
+            this.lbRequireDate.Name = "lbRequireDate";
+            this.lbRequireDate.Size = new System.Drawing.Size(81, 15);
+            this.lbRequireDate.TabIndex = 5;
+            this.lbRequireDate.Text = "Required Date";
+            // 
+            // lbShippedDate
+            // 
+            this.lbShippedDate.AutoSize = true;
+            this.lbShippedDate.Location = new System.Drawing.Point(12, 300);
+            this.lbShippedDate.Name = "lbShippedDate";
+            this.lbShippedDate.Size = new System.Drawing.Size(77, 15);
+            this.lbShippedDate.TabIndex = 6;
+            this.lbShippedDate.Text = "Shipped Date";
+            // 
+            // lbFreight
+            // 
+            this.lbFreight.AutoSize = true;
+            this.lbFreight.Location = new System.Drawing.Point(12, 346);
+            this.lbFreight.Name = "lbFreight";
+            this.lbFreight.Size = new System.Drawing.Size(44, 15);
+            this.lbFreight.TabIndex = 7;
+            this.lbFreight.Text = "Freight";
+            // 
+            // txtOrderID
+            // 
+            this.txtOrderID.Location = new System.Drawing.Point(12, 146);
+            this.txtOrderID.Name = "txtOrderID";
+            this.txtOrderID.ReadOnly = true;
+            this.txtOrderID.Size = new System.Drawing.Size(140, 23);
+            this.txtOrderID.TabIndex = 8;
+            // 
+            // txtMemberID
+            // 
+            this.txtMemberID.Location = new System.Drawing.Point(12, 190);
+            this.txtMemberID.Name = "txtMemberID";
+            this.txtMemberID.ReadOnly = true;
+            this.txtMemberID.Size = new System.Drawing.Size(140, 23);
+            this.txtMemberID.TabIndex = 9;
+            // 
+            // txtOrderDate
+            // 
+            this.txtOrderDate.Location = new System.Drawing.Point(12, 234);
+            this.txtOrderDate.Name = "txtOrderDate";
+            this.txtOrderDate.ReadOnly = true;
+            this.txtOrderDate.Size = new System.Drawing.Size(140, 23);
+            this.txtOrderDate.TabIndex = 10;
+            // 
+            // txtRequiredDate
+            // 
+            this.txtRequiredDate.Location = new System.Drawing.Point(12, 278);
+            this.txtRequiredDate.Name = "txtRequiredDate";
+            this.txtRequiredDate.ReadOnly = true;
+            this.txtRequiredDate.Size = new System.Drawing.Size(140, 23);
+            this.txtRequiredDate.TabIndex = 11;
+            // 
+            // txtShippedDate
+            // 
+            this.txtShippedDate.Location = new System.Drawing.Point(12, 320);
+            this.txtShippedDate.Name = "txtShippedDate";
+            this.txtShippedDate.ReadOnly = true;
+            this.txtShippedDate.Size = new System.Drawing.Size(140, 23);
+            this.txtShippedDate.TabIndex = 12;
+            // 
+            // txtFreight
+            // 
+            this.txtFreight.Location = new System.Drawing.Point(12, 364);
+            this.txtFreight.Name = "txtFreight";
+            this.txtFreight.ReadOnly = true;
+            this.txtFreight.Size = new System.Drawing.Size(140, 23);
+            this.txtFreight.TabIndex = 13;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(623, 115);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(134, 23);
+            this.btnSearch.TabIndex = 14;
+            this.btnSearch.Text = "Search By Order ID";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(763, 115);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(143, 23);
+            this.txtSearch.TabIndex = 16;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(533, 409);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 17;
+            this.btnAdd.Text = "Add ";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(647, 409);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 18;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(763, 409);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 19;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Sitka Small", 26.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(271, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(373, 52);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Order Management";
+            // 
+            // btnOrderDetail
+            // 
+            this.btnOrderDetail.Location = new System.Drawing.Point(183, 409);
+            this.btnOrderDetail.Name = "btnOrderDetail";
+            this.btnOrderDetail.Size = new System.Drawing.Size(178, 23);
+            this.btnOrderDetail.TabIndex = 21;
+            this.btnOrderDetail.Text = "Order Detail Management";
+            this.btnOrderDetail.UseVisualStyleBackColor = true;
+            this.btnOrderDetail.Click += new System.EventHandler(this.btnOrderDetail_Click);
+            // 
+            // frmOrderManagement
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(918, 464);
+            this.Controls.Add(this.btnOrderDetail);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtFreight);
+            this.Controls.Add(this.txtShippedDate);
+            this.Controls.Add(this.txtRequiredDate);
+            this.Controls.Add(this.txtOrderDate);
+            this.Controls.Add(this.txtMemberID);
+            this.Controls.Add(this.txtOrderID);
+            this.Controls.Add(this.lbFreight);
+            this.Controls.Add(this.lbShippedDate);
+            this.Controls.Add(this.lbRequireDate);
+            this.Controls.Add(this.lbOrderDate);
+            this.Controls.Add(this.lbMemberID);
+            this.Controls.Add(this.lbOrderID);
+            this.Controls.Add(this.btnView);
+            this.Controls.Add(this.dgvOrderList);
+            this.Name = "frmOrderManagement";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmOrderManagement";
+            this.Load += new System.EventHandler(this.frmOrderManagement_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOrderList)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DataGridView dgvOrderList;
+        private Button btnView;
+        private Label lbOrderID;
+        private Label lbMemberID;
+        private Label lbOrderDate;
+        private Label lbRequireDate;
+        private Label lbShippedDate;
+        private Label lbFreight;
+        private TextBox txtOrderID;
+        private TextBox txtMemberID;
+        private TextBox txtOrderDate;
+        private TextBox txtRequiredDate;
+        private TextBox txtShippedDate;
+        private TextBox txtFreight;
+        private Button btnSearch;
+        private TextBox txtSearch;
+        private Button btnAdd;
+        private Button btnDelete;
+        private Button btnClose;
+        private Label label1;
+        private Button btnOrderDetail;
+    }
+}
